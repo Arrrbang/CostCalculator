6e7312d800a8345cbc422d5b29b72fdbdc69b351
Arrrbang <arem0602@gmail.com>
2025-06-17 15:13:10 +0900

    Update homejs.js

--- js/homejs.js
+++ js/homejs.js
@@ -1,39 +1,3 @@
-  // -----------------------로그인 검증 및 검증 실패시 로그인 페이지 이동------------------------------
-
-function redirectToLogin(message) {
-  alert(message);
-  localStorage.removeItem('token'); 
-  window.location.href = 'index.html'; 
-}
-
-function verifyToken() {
-  const token = localStorage.getItem('token'); 
-  if (!token) {
-    redirectToLogin('로그인이 해제되었습니다. 다시 로그인해주세요.');
-    return;
-  }
-
-  fetch('https://backend-beta-lemon.vercel.app/verifyToken', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${token}`, 
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (!data.success) {
-        redirectToLogin('로그인이 해제되었습니다. 다시 로그인해주세요.');
-      }
-    })
-    .catch(() => {
-      redirectToLogin('Error verifying token. Redirecting to login page.');
-    });
-}
-
-
-    verifyToken();
-//-------------------------------------------------------------------------------------------------    
 let currencySymbol = ""; // 화폐 단위를 저장할 변수
 let dataNonDiplomat = {};
 let dataDiplomat = {};
